1d49429652718ff162b6ac6e3ef454adf1cddfd9
giuspru <giuseppeprudente20@gmail.com>
2025-07-23 12:50:49 +0200

    :construction:

--- Single_qubit/polinomial.py
+++ Single_qubit/polinomial.py
@@ -15,6 +15,15 @@ def circuit(x, weights):
     qml.RY(weights[0], wires=0)
     qml.RZ(weights[1], wires=0)
     qml.RX(weights[2], wires=0)
+    qml.RY(weights[3], wires=0)
+    qml.RZ(weights[4], wires=0)
+    qml.RX(weights[5], wires=0)
+    qml.RY(weights[6], wires=0)
+    qml.RZ(weights[7], wires=0)
+    qml.RX(weights[8], wires=0)
+    qml.RY(weights[9], wires=0)
+    qml.RZ(weights[10], wires=0)
+    qml.RX(weights[11], wires=0)
     
     return qml.expval(qml.PauliZ(0))  # output ∈ [-1, 1]
 
@@ -35,17 +44,19 @@ def inverse_scale_output(y):
 # Funzione di costo (errore quadratico medio tra target e predizione scalata)
 def cost(weights, X, Y_true):
     Y_pred = np.array([scale_output(circuit(x, weights)) for x in X])
+    #return np.sqrt(np.mean((Y_pred - Y_true) ** 2))
     return np.mean((Y_pred - Y_true) ** 2)
 
 #Data:
-X_train = np.linspace(0, 1, 20)
+X_train = np.linspace(0, 1, 600)
 Y_train = target_function(X_train)
 
 #Initial weights:
-weights = np.random.normal(0, 1, 3, requires_grad=True)
+weights = np.random.normal(0, 1, 12, requires_grad=True)
 
 # Ottimizzatore
-opt = qml.GradientDescentOptimizer(stepsize=0.1)
+#opt = qml.GradientDescentOptimizer(stepsize=0.01)
+opt = qml.AdamOptimizer(0.1)
 epochs = 100
 
 for i in range(epochs):
@@ -53,6 +64,7 @@ for i in range(epochs):
     if i % 10 == 0:
         loss = cost(weights, X_train, Y_train)
         print(f"Epoch {i} - Loss: {loss:.4f}")
+        print(f"Weights: {weights}")
 
 # Testing
 X_test = np.linspace(0, 1, 100)
